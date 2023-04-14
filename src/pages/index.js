6d729996548985eb4e0ee56936abb691f061c4d0
Steven Schoebinger <steven.schoebinger@gmail.com>
2023-04-13 23:27:11 -0600

    different timers

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,13 @@ export default function Home() {
 	const [timeRemaining, setTimeRemaining] = useState(25 * 60); // 25 minutes in seconds
 	const [timerRunning, setTimerRunning] = useState(false);
 	const [timerId, setTimerId] = useState(null);
+	const [timerType, setTimerType] = useState('pomodoro');
+
+	const timerDurations = {
+		pomodoro: 25 * 60,
+		long: 15 * 60,
+		short: 5 * 60,
+	};
 
 	const startTimer = () => {
 		if (!timerRunning) {
@@ -27,7 +34,7 @@ export default function Home() {
 
 	const restartTimer = () => {
 		clearInterval(timerId);
-		setTimeRemaining(25 * 60);
+		setTimeRemaining(timerDurations[timerType]);
 		setTimerRunning(false);
 	};
 
@@ -39,11 +46,45 @@ export default function Home() {
 		}
 	}, [timeRemaining, timerId]);
 
+	useEffect(() => {
+		if (timerDurations[timerType]) {
+			setTimeRemaining(timerDurations[timerType]);
+			restartTimer(timerId);
+		}
+	}, [timerType]);
+
 	const minutes = Math.floor(timeRemaining / 60);
 	const seconds = timeRemaining % 60;
 
 	return (
 		<div className='flex flex-col items-center justify-center min-h-screen'>
+			<div className='flex space-x-10 mb-5'>
+				<button
+					className={`text-xl p-1 rounded border-2 ${
+						timerType === 'pomodoro' ? 'border-slate-500' : 'border-transparent'
+					}`}
+					onClick={() => setTimerType('pomodoro')}
+				>
+					Pomodoro
+				</button>
+				<button
+					className={`text-xl p-1 rounded border-2 ${
+						timerType === 'long' ? 'border-slate-500' : 'border-transparent'
+					}`}
+					onClick={() => setTimerType('long')}
+				>
+					Long Break
+				</button>
+				<button
+					className={`text-xl p-1 rounded border-2 ${
+						timerType === 'short' ? 'border-slate-500' : 'border-transparent'
+					}`}
+					onClick={() => setTimerType('short')}
+				>
+					Short Break
+				</button>
+			</div>
+
 			<div className='flex items-center justify-center'>
 				<span className='text-6xl'>
 					{minutes < 10 ? `0${minutes}` : minutes}
@@ -53,7 +94,7 @@ export default function Home() {
 					{seconds < 10 ? `0${seconds}` : seconds}
 				</span>
 			</div>
-			<div className='mt-8'>
+			<div className='mt-5'>
 				<StartButton onClick={startTimer} running={timerRunning} />
 				<div className='flex mt-5 justify-between'>
 					<RestartButton onClick={restartTimer} />
