0e9326bfa2f41aad31e5f3e4b4040de317d84507
stevenshow <steven.schoebinger@gmail.com>
2023-04-13 21:01:14 -0600

    center button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 import StartButton from '@/components/StartButton';
 
 export default function Home() {
-	return <StartButton />;
+	return (
+		<div className='flex items-center justify-center h-screen'>
+			<StartButton />
+		</div>
+	);
 }
