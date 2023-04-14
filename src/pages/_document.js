92c47e601e14916765db6880e884804e24ce9474
Steven Schoebinger <steven.schoebinger@gmail.com>
2023-04-14 00:16:56 -0600

    place title correctly

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,7 +8,6 @@ export default function Document() {
 					name='description'
 					content='A little Pomodoro timer for heightened effectiveness'
 				/>
-				<title>Steve&apos;s Pomodoro Timer</title>
 			</Head>
 			<body>
 				<Main />
