660d2857b650652fa4c26b7be709acef808816ec
stevenshow <steven.schoebinger@gmail.com>
2023-04-13 20:59:40 -0600

    add power button

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -1,3 +1,11 @@
-const StartButton = () => {};
+import { MdPowerSettingsNew } from 'react-icons/md';
+
+const StartButton = () => {
+	return (
+		<button className='w-16 h-16 rounded-full border-2 border-blue-500 flex items-center justify-center'>
+			<MdPowerSettingsNew className='text-blue-500 w-6 h-6' />
+		</button>
+	);
+};
 
 export default StartButton;
