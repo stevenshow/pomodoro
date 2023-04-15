6b00b2242814e8991041f9b1b24a0824d997003a
stevenshow <steven.schoebinger@gmail.com>
2023-04-14 19:51:44 -0600

    prettier formatting

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -1,15 +1,15 @@
-import { useState } from 'react';
-import { MdPowerSettingsNew } from 'react-icons/md';
+import { useState } from "react";
+import { MdPowerSettingsNew } from "react-icons/md";
 
 const StartButton = ({ onClick }) => {
-	return (
-		<button
-			className='w-48 h-48 rounded-full border-2 border-blue-500 flex items-center justify-center'
-			onClick={onClick}
-		>
-			<MdPowerSettingsNew className='text-blue-500 w-14 h-14' />
-		</button>
-	);
+  return (
+    <button
+      className="flex h-48 w-48 items-center justify-center rounded-full border-2 border-blue-500"
+      onClick={onClick}
+    >
+      <MdPowerSettingsNew className="h-14 w-14 text-blue-500" />
+    </button>
+  );
 };
 
 export default StartButton;
